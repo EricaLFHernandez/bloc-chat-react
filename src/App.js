1e09b7a6bd3211a215ef435c2e50495825fe042e
Erica Hernandez <EricaLFHernandez@gmail.com>
2018-05-18 10:34:48 -0400

    lists names of chatrooms

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
+
+var config = {
+    apiKey: "AIzaSyCC_HQsHrkW9AgPo7ejtIkpVRHayVo9_Uw",
+    authDomain: "bloc-chat-react-be2df.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-be2df.firebaseio.com",
+    projectId: "bloc-chat-react-be2df",
+    storageBucket: "bloc-chat-react-be2df.appspot.com",
+    messagingSenderId: "586184498276"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
